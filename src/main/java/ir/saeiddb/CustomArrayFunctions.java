7a2f209e61f6bb6754d4070165680f7f484107f8
saeiddb <saeiddb@gmail.com>
2021-03-15 10:26:28 +0330

    Feature: Find fixed sum pair numbers in a sorted list

--- src/main/java/ir/saeiddb/CustomArrayFunctions.java
+++ src/main/java/ir/saeiddb/CustomArrayFunctions.java
@@ -1,25 +1,49 @@
 package ir.saeiddb;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
-
 public class CustomArrayFunctions {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomArrayFunctions.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(CustomArrayFunctions.class);
 
-    public static List<Number> findDuplicatedNumbers(List<Number> numbers) {
-        Set<Number> uniqueNumbers = new HashSet();
-        List<Number> duplicates = new ArrayList<>();
-        for (Number number :
-                numbers) {
-            if (uniqueNumbers.contains(number)) {
-                duplicates.add(number);
-            } else {
-                uniqueNumbers.add(number);
-            }
-        }
-        return duplicates;
+  public static List<Number> findDuplicatedNumbers(List<Number> numbers) {
+    Set<Number> uniqueNumbers = new HashSet();
+    List<Number> duplicates = new ArrayList<>();
+    for (Number number : numbers) {
+      if (uniqueNumbers.contains(number)) {
+        duplicates.add(number);
+      } else {
+        uniqueNumbers.add(number);
+      }
     }
+    return duplicates;
+  }
+
+  public static List<Number[]> findFixedSumPairNumbersInSortedList(List<Number> sortedUniqueNumbers,
+      Double expectedSum) {
+    int first = 0;
+    int last = sortedUniqueNumbers.size() - 1;
+    List<Number[]> result = new ArrayList<>();
+    while (last > first) {
+      Double sum =
+          sortedUniqueNumbers.get(first).doubleValue() + sortedUniqueNumbers.get(last).doubleValue();
+      if (sum.equals(expectedSum)) {
+        LOGGER.debug("Expected sum: {} numbers are: {}, {} in indices: {}, {}", expectedSum,
+            sortedUniqueNumbers.get(first), sortedUniqueNumbers.get(last), first, last);
+        result.add(new Number[]{sortedUniqueNumbers.get(first), sortedUniqueNumbers.get(last)});
+        last--;
+        first++;
+      } else if (sum > expectedSum) {
+        last--;
+      } else {
+        first++;
+      }
+    }
+    return result;
+  }
 }
--- src/test/java/ir/saeiddb/CustomArrayFunctionTest.java
+++ src/test/java/ir/saeiddb/CustomArrayFunctionTest.java
@@ -1,24 +1,48 @@
 package ir.saeiddb;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class CustomArrayFunctionTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomArrayFunctionTest.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(CustomArrayFunctionTest.class);
 
-    private static final List<Number> NUMBERS_FOR_DUPLICATE_CHECKING = Arrays.asList(7, 4, 1, 9, 7, 5, 4, 1.0);
+  private static final List<Number> NUMBERS_FOR_DUPLICATE_CHECKING = Arrays
+      .asList(7, 4, 1, 9, 7, 5, 4, 1.0);
+  private static final List<Number> SORTED_NUMBERS_FOR_FIXED_SUM_CHECKING = Arrays
+      .asList(1, 3, 4, 6, 8, 11, 15, 23, 24, 58, 59, 62, 67, 69, 70, 78);
 
-    @Test
-    public void findDuplicatedNumbersTest() {
-        List<Number> expectedResult = Arrays.asList(4, 7);
-        List<Number> actualResult = CustomArrayFunctions.findDuplicatedNumbers(NUMBERS_FOR_DUPLICATE_CHECKING);
-        Assert.assertTrue(expectedResult.size() == actualResult.size() &&
-                expectedResult.containsAll(actualResult));
+  @Test
+  public void findDuplicatedNumbersTest() {
+    List<Number> expectedResult = Arrays.asList(4, 7);
+    List<Number> actualResult = CustomArrayFunctions
+        .findDuplicatedNumbers(NUMBERS_FOR_DUPLICATE_CHECKING);
+    Assert.assertTrue(expectedResult.size() == actualResult.size() &&
+        expectedResult.containsAll(actualResult));
+  }
+
+  @Test
+  public void findFixedSumPairNumbersInSortedArrayTest() {
+    List<Number[]> expectedResult = Arrays
+        .asList(new Number[]{3, 70}, new Number[]{6, 67}, new Number[]{11, 62}, new Number[]{69, 4},
+            new Number[]{15, 58});
+    List<Number[]> actualResult = CustomArrayFunctions
+        .findFixedSumPairNumbersInSortedList(SORTED_NUMBERS_FOR_FIXED_SUM_CHECKING, 73D);
+    Assert.assertTrue(expectedResult.size() == actualResult.size());
+    for (int i = 0; i < expectedResult.size(); i++) {
+      boolean sameAnswerWasFound = false;
+      for (int j = 0; j < actualResult.size(); j++) {
+        if (expectedResult.get(i).length == actualResult.get(j).length &&
+            Arrays.asList(expectedResult.get(i)).containsAll(Arrays.asList(actualResult.get(j)))) {
+          sameAnswerWasFound = true;
+          continue;
+        }
+      }
+      Assert.assertTrue(sameAnswerWasFound);
     }
+  }
 }
